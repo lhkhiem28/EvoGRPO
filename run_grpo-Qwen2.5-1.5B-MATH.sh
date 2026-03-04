d6ad11ba332db580e6768335edfc9e84ccc244d0
lhkhiem28 <lhkhiem28@gmail.com>
2026-03-03 21:21:18 -0500

    .

--- run_grpo-Qwen2.5-1.5B-MATH.sh
+++ run_grpo-Qwen2.5-1.5B-MATH.sh
@@ -141,4 +141,146 @@ python preprocess.py --make_crossover --task "AIME25"
 python preprocess.py --make_crossover --task "Minerva"
 python preprocess.py --make_crossover --task "GPQA"
 
-dos2unix eval.sh; bash eval.sh lhkhiem28/${experiment_name} --make_crossover; bash eval.sh lhkhiem28/${experiment_name} --make_crossover; bash eval.sh lhkhiem28/${experiment_name} --make_crossover
+# dos2unix eval.sh; bash eval.sh lhkhiem28/${experiment_name} --make_crossover; bash eval.sh lhkhiem28/${experiment_name} --make_crossover; bash eval.sh lhkhiem28/${experiment_name} --make_crossover
+
+project_name="EvoGRPO"
+experiment_name="Qwen2.5-1.5B-GRPO-2"
+
+train_files="['../EvoGRPO-datasets/MATH/train.parquet']"
+val_files="['../EvoGRPO-datasets/MATH/test.parquet']"
+
+PYTHONUNBUFFERED=1 python -m verl.trainer.main_ppo \
+    algorithm.adv_estimator=grpo \
+    data.train_files="$train_files" \
+    data.val_files="$val_files" \
+    data.train_batch_size=256 \
+    data.max_prompt_length=512 \
+    data.max_response_length=3072 \
+    data.filter_overlong_prompts=True \
+    data.truncation='error' \
+    actor_rollout_ref.model.path=lhkhiem28/Qwen2.5-1.5B-GRPO-evo-1 \
+    actor_rollout_ref.model.use_remove_padding=True \
+    actor_rollout_ref.model.enable_gradient_checkpointing=True \
+    actor_rollout_ref.actor.optim.lr=1e-6 \
+    actor_rollout_ref.actor.optim.lr_warmup_steps_ratio=0.1 \
+    actor_rollout_ref.actor.optim.warmup_style=cosine \
+    actor_rollout_ref.actor.use_kl_loss=True \
+    actor_rollout_ref.actor.kl_loss_type=low_var_kl \
+    actor_rollout_ref.actor.kl_loss_coef=0.001 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=128 \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=8 \
+    actor_rollout_ref.actor.policy_loss.loss_mode=vanilla \
+    actor_rollout_ref.rollout.name=vllm \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.8 \
+    actor_rollout_ref.rollout.max_num_batched_tokens=8192 \
+    actor_rollout_ref.rollout.n=8 \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=8 \
+    actor_rollout_ref.ref.fsdp_config.param_offload=True \
+    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=8 \
+    trainer.resume_mode='disable' \
+    trainer.val_before_train=False \
+    trainer.total_epochs=1 \
+    trainer.nnodes=1 \
+    trainer.n_gpus_per_node=4 \
+    trainer.logger=['console','wandb'] \
+    trainer.project_name="$project_name" \
+    trainer.experiment_name="$experiment_name" \
+    trainer.test_freq=28 \
+    trainer.save_freq=28 $@
+
+PYTHONUNBUFFERED=1 python -m verl.model_merger merge \
+    --backend fsdp \
+    --local_dir checkpoints/${project_name}/${experiment_name}/global_step_28/actor \
+    --target_dir checkpoints/${project_name}/${experiment_name}/global_step_28/actor/huggingface
+python test.py --repo_id lhkhiem28/${experiment_name} --folder_path checkpoints/${project_name}/${experiment_name}/global_step_28/actor/huggingface
+
+dos2unix eval.sh; bash eval.sh lhkhiem28/${experiment_name}; bash eval.sh lhkhiem28/${experiment_name}; bash eval.sh lhkhiem28/${experiment_name}
+
+python make_crossover.py --repo_id lhkhiem28/${experiment_name} --task "MATH" --split "train"
+python make_crossover.py --repo_id lhkhiem28/${experiment_name} --task "MATH"
+python make_crossover.py --repo_id lhkhiem28/${experiment_name} --task "AMC12"
+python make_crossover.py --repo_id lhkhiem28/${experiment_name} --task "OlympiadBench"
+python make_crossover.py --repo_id lhkhiem28/${experiment_name} --task "AIME24"
+python make_crossover.py --repo_id lhkhiem28/${experiment_name} --task "AIME25"
+python make_crossover.py --repo_id lhkhiem28/${experiment_name} --task "Minerva"
+python make_crossover.py --repo_id lhkhiem28/${experiment_name} --task "GPQA"
+python preprocess.py --make_crossover --task "MATH" --split "train"
+python preprocess.py --make_crossover --task "MATH"
+python preprocess.py --make_crossover --task "AMC12"
+python preprocess.py --make_crossover --task "OlympiadBench"
+python preprocess.py --make_crossover --task "AIME24"
+python preprocess.py --make_crossover --task "AIME25"
+python preprocess.py --make_crossover --task "Minerva"
+python preprocess.py --make_crossover --task "GPQA"
+
+project_name="EvoGRPO"
+experiment_name="Qwen2.5-1.5B-GRPO-evo-2"
+
+train_files="['../EvoGRPO-datasets/MATH/train_evo.parquet']"
+val_files="['../EvoGRPO-datasets/MATH/test_evo.parquet']"
+
+PYTHONUNBUFFERED=1 python -m verl.trainer.main_ppo \
+    algorithm.adv_estimator=grpo \
+    data.train_files="$train_files" \
+    data.val_files="$val_files" \
+    data.train_batch_size=256 \
+    data.max_prompt_length=6656 \
+    data.max_response_length=3072 \
+    data.filter_overlong_prompts=True \
+    data.truncation='error' \
+    actor_rollout_ref.model.path=lhkhiem28/Qwen2.5-1.5B-GRPO-2 \
+    actor_rollout_ref.model.use_remove_padding=True \
+    actor_rollout_ref.model.enable_gradient_checkpointing=True \
+    actor_rollout_ref.actor.optim.lr=1e-6 \
+    actor_rollout_ref.actor.optim.lr_warmup_steps_ratio=0.1 \
+    actor_rollout_ref.actor.optim.warmup_style=cosine \
+    actor_rollout_ref.actor.use_kl_loss=True \
+    actor_rollout_ref.actor.kl_loss_type=low_var_kl \
+    actor_rollout_ref.actor.kl_loss_coef=0.001 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=128 \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=8 \
+    actor_rollout_ref.actor.policy_loss.loss_mode=vanilla \
+    actor_rollout_ref.rollout.name=vllm \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.8 \
+    actor_rollout_ref.rollout.max_num_batched_tokens=16384 \
+    actor_rollout_ref.rollout.n=8 \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=8 \
+    actor_rollout_ref.ref.fsdp_config.param_offload=True \
+    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=8 \
+    trainer.resume_mode='disable' \
+    trainer.val_before_train=False \
+    trainer.total_epochs=1 \
+    trainer.nnodes=1 \
+    trainer.n_gpus_per_node=4 \
+    trainer.logger=['console','wandb'] \
+    trainer.project_name="$project_name" \
+    trainer.experiment_name="$experiment_name" \
+    trainer.test_freq=29 \
+    trainer.save_freq=29 $@
+
+PYTHONUNBUFFERED=1 python -m verl.model_merger merge \
+    --backend fsdp \
+    --local_dir checkpoints/${project_name}/${experiment_name}/global_step_29/actor \
+    --target_dir checkpoints/${project_name}/${experiment_name}/global_step_29/actor/huggingface
+python test.py --repo_id lhkhiem28/${experiment_name} --folder_path checkpoints/${project_name}/${experiment_name}/global_step_29/actor/huggingface
+
+dos2unix eval.sh; bash eval.sh lhkhiem28/${experiment_name}; bash eval.sh lhkhiem28/${experiment_name}; bash eval.sh lhkhiem28/${experiment_name}
+
+python make_crossover.py --repo_id lhkhiem28/${experiment_name} --task "MATH" --split "train"
+python make_crossover.py --repo_id lhkhiem28/${experiment_name} --task "MATH"
+python make_crossover.py --repo_id lhkhiem28/${experiment_name} --task "AMC12"
+python make_crossover.py --repo_id lhkhiem28/${experiment_name} --task "OlympiadBench"
+python make_crossover.py --repo_id lhkhiem28/${experiment_name} --task "AIME24"
+python make_crossover.py --repo_id lhkhiem28/${experiment_name} --task "AIME25"
+python make_crossover.py --repo_id lhkhiem28/${experiment_name} --task "Minerva"
+python make_crossover.py --repo_id lhkhiem28/${experiment_name} --task "GPQA"
+python preprocess.py --make_crossover --task "MATH" --split "train"
+python preprocess.py --make_crossover --task "MATH"
+python preprocess.py --make_crossover --task "AMC12"
+python preprocess.py --make_crossover --task "OlympiadBench"
+python preprocess.py --make_crossover --task "AIME24"
+python preprocess.py --make_crossover --task "AIME25"
+python preprocess.py --make_crossover --task "Minerva"
+python preprocess.py --make_crossover --task "GPQA"
+
+# dos2unix eval.sh; bash eval.sh lhkhiem28/${experiment_name} --make_crossover; bash eval.sh lhkhiem28/${experiment_name} --make_crossover; bash eval.sh lhkhiem28/${experiment_name} --make_crossover
